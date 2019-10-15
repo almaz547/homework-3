a3d8e918b65b87c6f624af033f9fb1d81a5046f6
almaz547 <almaz_xafizov@list.ru>
2019-10-15 11:58:56 +0300

    add.project

--- 3seq.py
+++ 3seq.py
@@ -1,5 +1,5 @@
 
-data_first = input('Введите елементы первого списка через запятую: ')
+'''data_first = input('Введите елементы первого списка через запятую: ')
 list_first = []
 while len(data_first):
     data_first = data_first.split(',')
@@ -21,7 +21,43 @@ while i < len(list_first):
         list_first.pop(i)
     else:
         i += 1
+print(list_first)'''
+
+
+data_first = input('Введите елементы первого списка через одинаковый разделитель: ')
+list_first = []
+while len(data_first):
+    for element in data_first:
+        if not element.isalnum():
+            sep = element
+            break
+    data_first = data_first.split(sep)
+    for element in data_first:
+        list_first.append(element)
+    data_first = input('Дополните первый список через одинаковый разделитель, или нажмите Enter: ')
 print(list_first)
+data_second = input('Введите второй список через одинаковый раздедитель: ')
+list_second = []
+while len(data_second):
+    for element in data_second:
+        if not element.isalnum():
+            sep = element
+            break
+    data_second = data_second.split(sep)
+    for element in data_second:
+        list_second.append(element)
+    data_second = input('Дополните второй список через одинаковый разделитель, или нажмите Enter: ')
+print(list_second)
+i = 0
+while i < len(list_first):
+    if list_first[i] in list_second:
+        list_first.pop(i)
+    else:
+        i += 1
+print(list_first)
+
+
+
 
 
 
